4c141bc4c853de4a19b108d6fcf74c9ea464363d
Anna M <anna@germ.network>
2026-03-24 09:48:19 -0700

    Rev dependencies

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/AtprotoTypes.git",
-			from: "0.0.3"
+			from: "0.0.4"
 		),
 		.package(
 			url: "https://github.com/germ-network/GermConvenience.git",
@@ -24,7 +24,7 @@ let package = Package(
 		),
 		.package(
 			url: "https://github.com/germ-network/AtprotoClient.git",
-			from: "0.0.3"
+			from: "0.0.4"
 		),
 	],
 	targets: [
