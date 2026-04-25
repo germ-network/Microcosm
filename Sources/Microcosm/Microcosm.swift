d0914715618c582e8d3af4c5eebf90d92bb82af9
Mark Xue <mark@germ.network>
2026-04-24 21:35:20 -0700

    use correct localized error requirement of errorDescription

--- Sources/Microcosm/Microcosm.swift
+++ Sources/Microcosm/Microcosm.swift
@@ -8,7 +8,7 @@ import Foundation
 import HTTPTypes
 
 public enum Microcosm {
-	enum Errors: Error {
+	enum Errors: LocalizedError {
 		case notImplemented
 		case unexpectedRecordType
 		case missingRecordValue
@@ -17,21 +17,19 @@ public enum Microcosm {
 		case couldntConstructUrl
 		case nonHTTPResponse
 		case requestFailed(responseStatus: HTTPResponse.Status, error: String)
-	}
-}
 
-extension Microcosm.Errors: LocalizedError {
-	var localizedDescription: String {
-		switch self {
-		case .notImplemented: "Method not implemented"
-		case .unexpectedRecordType: "Unexpected record type"
-		case .missingRecordValue: "Missing record value"
-		case .failedToDecodeRecord: "Failed to decode record"
-		case .improperServiceUrl: "Improper service URL"
-		case .couldntConstructUrl: "Couldn't construct URL"
-		case .nonHTTPResponse: "Request failed with non-HTTP response"
-		case .requestFailed(let responseCode, let errorString):
-			"Request failed with response code: \(responseCode), error \(errorString)"
+		var errorDescription: String? {
+			switch self {
+			case .notImplemented: "Method not implemented"
+			case .unexpectedRecordType: "Unexpected record type"
+			case .missingRecordValue: "Missing record value"
+			case .failedToDecodeRecord: "Failed to decode record"
+			case .improperServiceUrl: "Improper service URL"
+			case .couldntConstructUrl: "Couldn't construct URL"
+			case .nonHTTPResponse: "Request failed with non-HTTP response"
+			case .requestFailed(let responseCode, let errorString):
+				"Request failed with response code: \(responseCode), error \(errorString)"
+			}
 		}
 	}
 }
