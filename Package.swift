24bf366bcc8f9fc0c6769dd1e70bcc58fb7a9122
Mark Xue <mark@germ.network>
2026-05-19 21:59:59 -0700

    bump dependencies to consistent use of safe httpresponse init, narrow http imports

--- Package.swift
+++ Package.swift
@@ -17,15 +17,18 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/AtprotoTypes.git",
-			from: "0.4.1"
+//			from: "0.4.1"
+			branch: "fix/reduce-imports"
 		),
 		.package(
 			url: "https://github.com/germ-network/GermConvenience.git",
-			from: "0.1.5"
+//			from: "0.1.5"
+			branch: "fix/http-scheme-precondition"
 		),
 		.package(
 			url: "https://github.com/germ-network/AtprotoClient.git",
-			from: "0.5.4"
+//			from: "0.5.4"
+			branch: "fix/imports"
 		),
 		.package(url: "https://github.com/apple/swift-http-types.git", from: "1.5.1"),
 	],
